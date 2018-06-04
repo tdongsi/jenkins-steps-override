044eb37c1f2c2b1be5c49d0d9375e0e8fdbee742
tdongsi <dongsi.tuecuong@gmail.com>
2018-06-03 18:21:28 -0700

    First Pipeline unit tests.

---
+++ test/vars/BuildWrapperTest.groovy
@@ -0,0 +1,63 @@
+package vars
+
+import com.lesfurets.jenkins.unit.BaseRegressionTest
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * Created by tdongsi on 6/3/18.
+ */
+class BuildWrapperTest extends BaseRegressionTest {
+
+  @Override
+  @Before
+  public void setUp() throws Exception {
+    super.setUp();
+
+    callStackPath = "test/vars/callstacks/"
+
+    /*
+    // A more proper variable mocking
+    binding.setVariable('scm', [
+        $class: 'GitSCM',
+        branches: [[name: 'master']],
+        doGenerateSubmoduleConfigruations: false,
+        extensions: [],
+        submoduleCfg: [],
+        userRemoteConfigs: [[url: "/var/git-repo"]]
+    ])
+    */
+    binding.setVariable('scm', [:])
+
+    helper.registerAllowedMethod('library', [String.class], null)
+  }
+
+  @Test
+  public void configured() throws Exception {
+    /*
+    Represent the call:
+    buildWrapperDemo {
+      settings = "dummy.xml"
+    }
+    */
+    def script = loadScript('vars/buildWrapper.groovy')
+    script.call({settings = "dummy.xml"})
+
+    // printCallStack()
+    testNonRegression("configured")
+  }
+
+  @Test
+  public void default_value() throws Exception {
+    /*
+    Represent the call:
+    buildWrapperDemo {
+    }
+    */
+    def script = loadScript('vars/buildWrapper.groovy')
+    script.call({})
+
+    // printCallStack()
+    testNonRegression("default")
+  }
+}
